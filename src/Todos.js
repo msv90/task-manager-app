b40794e51e98cb237ac1bfea26d7941594eb96ef
Mike <mike.developer90@gmail.com>
2020-04-14 10:27:52 +0300

    Delete todo.

---
+++ src/Todos.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './Todos.css';
+
+const Todos = ({todos, deleteTodo}) => {
+
+    const todoList = todos.length ? (
+        todos.map(todo => {
+            return(
+                <div className="collection-item" key={todo.id}>
+                    <span>{todo.content}</span>
+                    <span className="item-delete-icon right" onClick={() => {deleteTodo(todo.id)}}>X</span>
+                </div>
+            )
+        })
+    ) : (
+        <p className="center">You have no todo's left, yay!</p>
+    );
+
+    return(
+        <div className="todos collection">
+            {todoList}
+        </div>
+    );
+
+};
+
+export default Todos;
